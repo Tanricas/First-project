712009e82c687ca3640ab776758e347ccbe9abfe
Tanricas <150886728+Tanricas@users.noreply.github.com>
2023-11-16 12:03:51 +0500

    Add files via upload

---
+++ ЛР1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите кол-во строк: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите кол-во столбцов: ");
+int n = int.Parse(Console.ReadLine());
+double[,] matrix = new double[n, m];
+Console.WriteLine("Введите элементы для матрицы: ");
+for (int i = 0; i<n ; i++)
+{
+    string[] r = Console.ReadLine().Split("");
+    for(int j = 0; j<m;j++)
+        matrix[i, j] = double.Parse(r[j]);
+}
+Console.WriteLine("Готовая матрица: ");
+for(int j = 0; j<m; j++)
+{
+    for (int i = 0; i < n; i++)
+    {
+        Console.WriteLine(matrix[i, j] + "\t");
+    }
+}
+
